2dd4cf171b304fae8959737cb92a7a29c25ee7a4
Manuel Familia <manuelgfx@gmail.com>
2016-02-07 09:40:37 -0500

    Update mongodb library

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Mongo.Ecto.Mixfile do
 
   defp deps do
     [
-      {:mongodb, "~> 0.1"},
+      {:mongodb, "~> 0.1.1"},
       {:ecto, "~> 1.0.0"},
       {:dialyze, "~> 0.2.0", only: :dev},
       {:excoveralls, "~> 0.3.11", only: :test},
